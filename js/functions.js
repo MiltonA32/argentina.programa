788b10d5d8d6d410777d457fedff8d7401a8573c
MiltonA32 <125771552+MiltonA32@users.noreply.github.com>
2023-02-18 19:56:53 -0300

    create functions

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+document.getElementById("fadicad").onclick = function () {
+    document.getElementById("modalFadicad").style.display = "block"
+}
+
+document.getElementById("cierre").onclick = function () {
+    document.getElementById("modalFadicad").style.display = "none"
+}
+
+document.getElementById("displan").onclick = function () {
+    document.getElementById("modalDisplan").style.display = "block"
+}
+
+document.getElementById("cierre2").onclick = function () {
+    document.getElementById("modalDisplan").style.display = "none"
+}
